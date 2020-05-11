a2ca74f6713ed595c22465c7e3df5ca2e54285e0
Chao Ma <chao_ma@whu.edu.cn>
2020-05-11 23:17:28 +0800

    #1 Two Sum

---
+++ LeetCode/T1_TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T1_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> keyValues = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int temp = target - nums[i];
+                if (keyValues.ContainsKey(temp))
+                {
+                    return new int[] { i, keyValues[temp] };
+                }
+                keyValues.Add(nums[i], i);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LeetCodeTests/T1_TwoSumTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T1_TwoSumTests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+            T1_TwoSum t1_TwoSum = new T1_TwoSum();
+
+            var result = t1_TwoSum.TwoSum(nums, target);
+
+            Assert.AreNotEqual(null, result);
+        }
+    }
+}
