e40798a5b7a0e0c547b5f0658c4df4f0704b9376
chao.ma <chao_ma@whu.edu.cn>
2020-09-13 14:41:46 +0800

    fix: #39 Combination Sum test

--- LeetCodeTests/T39_CombinationSumTests.cs
+++ LeetCodeTests/T39_CombinationSumTests.cs
@@ -10,13 +10,15 @@ namespace LeetCode.Tests
     public class T39_CombinationSumTests
     {
         [TestMethod()]
-        public void CombinationSumTest()
+        [DataRow(new int[] { 2, 3, 5 }, 8, "[[2,2,2,2],[2,3,3],[3,5]]", DisplayName ="test1")]
+        [DataRow(new int[] { 2, 3, 6, 7 }, 7, "[[2,2,3],[7]]", DisplayName = "test2")]
+        public void CombinationSumTest(int[] candidates, int target, string expect)
         {
-            int[] candidates = new int[] { 2, 3, 5 };//{ 2, 3, 6, 7 };
-            int target = 8;//7;
+            //int[] candidates = new int[] { 2, 3, 5 };//{ 2, 3, 6, 7 };
+            //int target = 8;//7;
             T39_CombinationSum t39_CombinationSum = new T39_CombinationSum();
             var result = t39_CombinationSum.CombinationSum(candidates, target);
-            Assert.AreNotEqual("[[],[]]", result, result);
+            Assert.AreEqual(expect, result, "fail");
         }
     }
 }
