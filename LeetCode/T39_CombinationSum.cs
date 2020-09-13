34930facf5ca4199e0a8825900b3527068bbbd9e
chao.ma <chao_ma@whu.edu.cn>
2020-09-13 13:59:07 +0800

    #39 Combination Sum

---
+++ LeetCode/T39_CombinationSum.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T39_CombinationSum
+    {
+        /// <summary>
+        /// 搜索回溯；类似的题转化成空间树
+        /// </summary>
+        /// <param name="candidates"></param>
+        /// <param name="target"></param>
+        public string CombinationSum(int[] candidates, int target)
+        {
+            List<int[]> result = new List<int[]>();
+            List<int> combine = new List<int>();
+            DFS(candidates, target, combine, 0, result);
+            return JsonConvert.SerializeObject(result);
+        }
+        /// <summary>
+        /// DFS深度优先搜索；BFS广度优先搜索；
+        /// </summary>
+        /// <param name="candidates"></param>
+        /// <param name="target"></param>
+        /// <param name="combine"></param>
+        /// <param name="index"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private void DFS(int[] candidates, int target, List<int> combine, int index, List<int[]> result)
+        {
+            if (index == candidates.Length)
+                return ;
+
+            if (target == 0)
+            {
+                result.Add(combine.ToArray());
+                return;
+            }
+
+            //结果数组combine中添加当前索引数
+            int target_new =target - candidates[index];
+            if (target_new >= 0)
+            {
+                combine.Add(candidates[index]);
+                DFS(candidates, target_new, combine, index, result);
+                combine.RemoveAt(combine.Count - 1);
+            }
+
+            //跳过当前index数
+            DFS(candidates, target, combine, index + 1, result);
+        }
+    }
+}
---
+++ LeetCodeTests/T39_CombinationSumTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T39_CombinationSumTests
+    {
+        [TestMethod()]
+        public void CombinationSumTest()
+        {
+            int[] candidates = new int[] { 2, 3, 5 };//{ 2, 3, 6, 7 };
+            int target = 8;//7;
+            T39_CombinationSum t39_CombinationSum = new T39_CombinationSum();
+            var result = t39_CombinationSum.CombinationSum(candidates, target);
+            Assert.AreNotEqual("[[],[]]", result, result);
+        }
+    }
+}
