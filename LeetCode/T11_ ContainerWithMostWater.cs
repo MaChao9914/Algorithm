e442e5638a9bf8aaf2080c3ba1a2357cb6d5de60
chao.ma <chao_ma@whu.edu.cn>
2020-09-20 20:34:59 +0800

    #11 Container With Most Water

---
+++ LeetCode/T11_ ContainerWithMostWater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T11__ContainerWithMostWater
+    {
+        /// <summary>
+        /// 双指针
+        /// 时间复杂度O(n);空间复杂度O(1)
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxContainer(int[] height)
+        {
+            int result = 0;
+            int left = 0, right = height.Length - 1;
+
+            while(left < right)
+            {
+                int temp = Math.Min(height[left], height[right]) * (right - left);
+                result = Math.Max(result, temp);
+                if (height[left] <= height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/T11__ContainerWithMostWaterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T11__ContainerWithMostWaterTests
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        public void ContainerTest(int[] height, int expected)
+        {
+            T11__ContainerWithMostWater t11__ContainerWithMostWater = new T11__ContainerWithMostWater();
+            var result = t11__ContainerWithMostWater.MaxContainer(height);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
