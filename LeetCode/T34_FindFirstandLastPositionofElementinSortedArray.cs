eca1bc1b47d20ab60c0733e02ac193408e5f4344
chao.ma <chao_ma@whu.edu.cn>
2020-10-01 20:32:27 +0800

    #34  Find First and Last Position of Element in Sorted Array

---
+++ LeetCode/T34_FindFirstandLastPositionofElementinSortedArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T34_FindFirstandLastPositionofElementinSortedArray
+    {
+        /// <summary>
+        /// 二分搜索左右边界
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int[] result = new int[] { -1, -1 };
+            int length = nums.Length;
+            if (length == 0)
+            {
+                return result;
+            }
+            result[0] = Search(nums, target, true);
+            result[1] = Search(nums, target, false);
+            return result;
+        }
+
+        private  int Search(int[] nums, int target, bool leftPosition)
+        {
+            int left = 0;
+            int rigth = nums.Length;
+            while (left < rigth)
+            {
+                int m = (left + rigth) / 2;
+                if (nums[m] == target)
+                {
+                    if (leftPosition)//搜索左边界
+                    {
+                        rigth = m;
+                    }
+                    else
+                    {
+                        left = m + 1;
+                    }
+                }
+                else if(nums[m] > target)
+                {
+                    rigth = m;
+                }
+                else if (nums[m] < target)
+                {
+                    left = m + 1;
+                }
+            }
+
+            if (leftPosition)//左边界返回值
+            {
+                if (left == nums.Length)
+                {
+                    return -1;
+                }
+                return nums[left] == target ? left : -1;
+            }
+            else
+            {
+                if (left == 0)
+                {
+                    return -1;
+                }
+                return nums[left - 1] == target ? left - 1 : -1;
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/T34_FindFirstandLastPositionofElementinSortedArrayTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T34_FindFirstandLastPositionofElementinSortedArrayTests
+    {
+        [TestMethod()]
+        //[DataRow(new int[] { 5, 7, 7, 8, 8, 10 }, 8, new int[] { 3, 4 }, DisplayName = "test1 failed")]
+        [DataRow(new int[] { 5, 7, 7, 8, 8, 10 }, 6, new int[] { -1, -1 }, DisplayName = "test2 failed")]
+        public void SearchRangeTest(int[] nums, int target, int[] expected)
+        {
+            T34_FindFirstandLastPositionofElementinSortedArray t34_FindFirstandLast = new T34_FindFirstandLastPositionofElementinSortedArray();
+            var result = t34_FindFirstandLast.SearchRange(nums, target);
+            bool isEqual = expected.SequenceEqual(result);
+            Assert.AreEqual(true, isEqual);
+        }
+    }
+}
