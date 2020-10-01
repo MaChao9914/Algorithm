6c55c0a59bea5860ba5461d06b6a9a1c08902d23
chao.ma <chao_ma@whu.edu.cn>
2020-10-01 11:13:06 +0800

    #31 Next Permutation

---
+++ LeetCode/T31_NextPermutation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T31_NextPermutation
+    {
+        public void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2;
+            while (i >=  0 && nums[i] >= nums[i+1])
+            {
+                i--;
+            }
+
+            if (i > 0)
+            {
+                int j = nums.Length - 1;
+                while (j > i && nums[j] <= nums[i])
+                {
+                    j--;
+                }
+                Swap(nums, i, j);
+            }
+
+            Array.Reverse(nums, i + 1, nums.Length - i - 1);
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            var temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
---
+++ LeetCodeTests/T31_NextPermutationTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T31_NextPermutationTests
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 1, 3, 2 })]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        public void NextPermutationTest(int[] nums, int[] targets)
+        {
+            T31_NextPermutation t31_NextPermutation = new T31_NextPermutation();
+            t31_NextPermutation.NextPermutation(nums);
+            var isEqual= targets.SequenceEqual(nums);
+            Assert.AreEqual(true, isEqual);
+        }
+    }
+}
