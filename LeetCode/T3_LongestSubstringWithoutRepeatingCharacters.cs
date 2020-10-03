57d178e62955bdf946d19070a39a48c76497eb3f
chao.ma <chao_ma@whu.edu.cn>
2020-10-03 11:50:01 +0800

    #3  Longest Substring Without Repeating Characters

---
+++ LeetCode/T3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T3_LongestSubstringWithoutRepeatingCharacters
+    {
+        /// <summary>
+        /// 滑动窗口
+        /// 注意测试用例3
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int LengthOfLongestSubstring(string s)
+        {
+            int length = s.Length;
+            int maxSubLength = 0;
+            Dictionary<char, int> window = new Dictionary<char, int>();
+            int left = 0;
+            for (int right = 0; right < length; right++)
+            {
+                var c1 = s[right];
+                if (window.ContainsKey(c1))
+                    window[c1]++;
+                else
+                    window.Add(c1, 1);
+
+                while(window[c1] > 1)
+                {
+                    var c2 = s[left];
+                    window[c2]--;
+                    left++;
+                }
+                maxSubLength = Math.Max(maxSubLength, right + 1 - left);
+            }
+
+            return maxSubLength;
+        }
+
+        /// <summary>
+        /// 使用linq
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int LengthOfLongestSubstring2(string s)
+        {
+            int length = s.Length;
+            int maxSubLength = 0;
+            List<char> window = new List<char>();
+            for (int right = 0; right < length; right++)
+            {
+                var c = s[right];
+                if (window.Contains(c))
+                {
+                    //Linq
+                    window = window.Skip(window.IndexOf(c) + 1).ToList();
+                }
+                window.Add(c);
+                maxSubLength = Math.Max(maxSubLength, window.Count);
+            }
+
+            return maxSubLength;
+        }
+    }
+}
---
+++ LeetCodeTests/T3_LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T3_LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [TestMethod()]
+        [DataRow("abcabcbb", 3)]
+        [DataRow("bbbbb", 1)]
+        [DataRow("pwwkew", 3)]
+        public void LengthOfLongestSubstringTest(string s, int ecpected)
+        {
+            T3_LongestSubstringWithoutRepeatingCharacters t3_LongestSubstring = new T3_LongestSubstringWithoutRepeatingCharacters();
+            var result = t3_LongestSubstring.LengthOfLongestSubstring(s);
+            Assert.AreEqual(ecpected, result);
+        }
+    }
+}
