d7852baf72733b52d40465987ed2c884dda04361
chao.ma <chao_ma@whu.edu.cn>
2020-10-04 10:47:35 +0800

    #49 Group Anagrams

---
+++ LeetCode/T49_ GroupAnagrams.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T49__GroupAnagrams
+    {
+        /// <summary>
+        /// 排序数组分类
+        /// 字母异位词就是两个字符串中的字母都是一样的，只不过顺序被打乱了
+        /// 算法复杂度 O(nklogk);空间复杂度O(nk)
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public IList<IList<string>> GroupAnagrams1(string[] strs)
+        {
+            if (strs == null && strs.Length == 0)
+                return new List<IList<string>>();
+            Dictionary<string, List<string>> pairs = new Dictionary<string, List<string>>();
+            foreach (var str in strs)
+            {
+                var chars = str.ToCharArray();
+                Array.Sort(chars);
+                var keyStr = new string(chars);
+                
+                if (!pairs.ContainsKey(keyStr))
+                {
+                    List<string> vs = new List<string>();
+                    pairs.Add(keyStr, vs);
+                }
+                pairs[keyStr].Add(str);
+            }
+            List<IList<string>> ans = new List<IList<string>>();
+            ans.AddRange(pairs.Values);
+            return ans;
+        }
+
+        /// <summary>
+        /// 按计数分类
+        /// 算法复杂度 O(nk);空间复杂度O(nk)
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public IList<IList<string>> GroupAnagrams2(string[] strs)
+        {
+            if (strs == null && strs.Length == 0)
+                return new List<IList<string>>();
+            Dictionary<string, List<string>> pairs = new Dictionary<string, List<string>>();
+
+            foreach (var str in strs)
+            {
+                char[] chars = new char[26];
+                foreach (var c in str.ToCharArray())
+                {
+                    chars[c - 'a']++;
+                }
+                string keyStr = new string(chars);
+                if (!pairs.ContainsKey(keyStr))
+                {
+                    List<string> vs = new List<string>();
+                    pairs.Add(keyStr, vs);
+                }
+                pairs[keyStr].Add(str);
+            }
+            List<IList<string>> ans = new List<IList<string>>();
+            ans.AddRange(pairs.Values);
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/T49__GroupAnagramsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T49__GroupAnagramsTests
+    {
+        [TestMethod()]
+        [DataRow(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" })]
+        public void GroupAnagrams1Test(string[] strs)
+        {
+            T49__GroupAnagrams t49__Group = new T49__GroupAnagrams();
+            var result = t49__Group.GroupAnagrams1(strs);
+        }
+
+        [TestMethod()]
+        [DataRow(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" })]
+        public void GroupAnagrams2Test(string[] strs)
+        {
+            T49__GroupAnagrams t49__Group = new T49__GroupAnagrams();
+            var result = t49__Group.GroupAnagrams2(strs);
+        }
+    }
+}
