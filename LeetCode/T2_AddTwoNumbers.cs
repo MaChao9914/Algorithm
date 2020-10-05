89495515dde207645f290bfd7015b38b56514727
chao.ma <chao_ma@whu.edu.cn>
2020-10-05 16:37:30 +0800

    #3 Add Two Numbers

---
+++ LeetCode/T2_AddTwoNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class T2_AddTwoNumbers
+    {
+        /// <summary>
+        /// 模拟10进制数字相加
+        /// 注意l1和l2是逆序的，所以对应位置可以直接相加
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode ans = new ListNode(0);
+            ListNode current = ans;
+
+            while (l1 != null || l2 != null)
+            {
+                int val1 = l1?.val ?? 0;
+                int val2 = l2?.val ?? 0;
+                int value = (val1 + val2 + current.val) % 10;
+                int carray = (val1 + val2 + current.val) / 10;
+                current.val = value;
+
+                if (l1 != null)
+                    l1 = l1.next;
+                if (l2 != null)
+                    l2 = l2.next;
+
+                if (l1 != null || l2 != null || carray > 0)
+                {
+                    current.next = new ListNode(carray);
+                    current = current.next;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ LeetCodeTests/T2_AddTwoNumbersTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T2_AddTwoNumbersTests
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8 }, DisplayName = "test1")]
+        [DataRow(new int[] { 0 }, new int[] { 0 }, new int[] { 0 }, DisplayName = "test2")]
+        [DataRow(new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 }, new int[] { 8, 9, 9, 9, 0, 0, 0, 1 }, DisplayName = "test3")]
+        public void AddTwoNumbersTest(int[] a1, int[] a2, int[] expected)
+        {
+            T2_AddTwoNumbers t2_AddTwo = new T2_AddTwoNumbers();
+
+            var l1 = GetListNode(a1);
+            var l2 = GetListNode(a2);
+
+            var ans = t2_AddTwo.AddTwoNumbers(l1, l2);
+            var actual = ListNodeToArray(ans);
+            bool isEqual = expected.SequenceEqual(actual);
+            Assert.AreEqual(true, isEqual);
+        }
+
+        private int[] ListNodeToArray(ListNode listNode)
+        {
+            List<int> ans = new List<int>();
+            ListNode current = listNode;
+            while(current != null)
+            {
+                var value = current.val;
+                ans.Add(value);
+                current = current.next;
+            }
+            return ans.ToArray();
+        }
+
+        private ListNode GetListNode(int[] array)
+        {
+            ListNode ans = new ListNode(0);
+            ListNode current = ans;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                current.val = array[i];
+                if (i < array.Length - 1)
+                {
+                    current.next = new ListNode(0);
+                    current = current.next;
+                }
+            }
+            return ans;
+        }
+    }
+}
