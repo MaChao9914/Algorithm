caec0dce38a6de7cfbcb6225f811e402bf15f15d
chao.ma <chao_ma@whu.edu.cn>
2020-10-07 17:05:11 +0800

    #23 Merge k Sorted Lists

---
+++ LeetCode/T23_MergekSortedLists.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T23_MergekSortedLists
+    {
+        /// <summary>
+        /// 顺序合并
+        /// </summary>
+        /// <param name="lists"></param>
+        /// <returns></returns>
+        public ListNode MergeKLists1(ListNode[] lists)
+        {
+            ListNode ans = null;
+
+            foreach (var item in lists)
+            {
+                ans = MergeTwoLists(ans, item);
+            }
+            return ans;
+        }
+
+        /// <summary>
+        /// 分而治之
+        /// </summary>
+        /// <param name="lists"></param>
+        /// <returns></returns>
+        public ListNode MergeKLists2(ListNode[] lists)
+        {
+            ListNode ans = null;
+            if (lists == null || lists.Length == 0)
+            {
+                return ans;
+            }
+
+            ans = MergeTwoLists(lists, 0, lists.Length - 1);
+
+            return ans;
+        }
+
+        public ListNode MergeTwoLists(ListNode[] lists, int left, int right)
+        {
+            if (left == right)
+                return lists[left];
+
+            int mid = (left + right) / 2;
+            ListNode listNode1 = MergeTwoLists(lists, left, mid);
+            ListNode listNode2 = MergeTwoLists(lists, mid + 1, right);
+            return MergeTwoLists(listNode1, listNode2);
+        }
+
+        public ListNode MergeTwoLists(ListNode a, ListNode b)
+        {
+            if (a == null || b == null)
+                return a ?? b;
+
+            ListNode head = new ListNode(0);
+            ListNode current = head;
+            while(a != null && b != null)
+            {
+                if (a.val < b.val)
+                {
+                    current.next = a;
+                    a = a.next;
+                }
+                else
+                {
+                    current.next = b;
+                    b = b.next;
+                }
+                current = current.next;
+            }
+
+            current.next = a ?? b;
+            return head.next;
+        }
+    }
+}
---
+++ LeetCodeTests/T23_MergekSortedListsTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T23_MergekSortedListsTests
+    {
+        [TestMethod()]
+        public void MergeKListsTest()
+        {
+            var input1 = new int[][] { new int[] { 1, 4, 5 }, new int[] { 1, 3, 4 }, new int[] { 2, 6 } };
+            var output1 = new int[] { 1, 1, 2, 3, 4, 4, 5, 6 };
+            var actual = Merge(input1);
+            bool isEqual = output1.SequenceEqual(actual);
+            Assert.AreEqual(true, isEqual, "test1");
+
+            var intput2 = new int[0][] { };
+            var output2 = new int[0];
+            actual = Merge(intput2);
+            isEqual = output2.SequenceEqual(actual);
+            Assert.AreEqual(true, isEqual, "test2");
+
+            var intput3 = new int[1][] { new int[0]{ } };
+            var output3 = new int[0];
+            var actual3 = Merge(intput3);
+            isEqual = output3.SequenceEqual(actual3);
+            Assert.AreEqual(true, isEqual, "test3");
+        }
+
+        public static int[] Merge(int[][] input)
+        {
+            var lists = GetListNodes(input);
+            T23_MergekSortedLists t23_MergekSortedLists = new T23_MergekSortedLists();
+            
+            var ans = t23_MergekSortedLists.MergeKLists2(lists);
+            
+            var actual = T2_AddTwoNumbersTests.ListNodeToArray(ans);
+            return actual;
+        }
+
+        public static ListNode[] GetListNodes(int[][] input)
+        {
+            List<ListNode> lists = new List<ListNode>();
+
+            foreach (var item in input)
+            {
+                var temp = T2_AddTwoNumbersTests.GetListNode(item);
+                if (temp.val == 0 && temp.next == null)
+                    continue;
+                lists.Add(temp);
+            }
+
+            return lists.ToArray();
+        }
+    }
+}
