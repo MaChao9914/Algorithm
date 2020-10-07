06934af7d48d2f75a926bc9c3b0e822ebfd2725e
chao.ma <chao_ma@whu.edu.cn>
2020-10-07 22:57:57 +0800

    #142  Linked List Cycle

---
+++ LeetCode/T142_LinkedListCycleII.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class T142_LinkedListCycleII
+    {
+        /// <summary>
+        /// 哈希表
+        /// 算法复杂度：O(n)；空间复杂度：O(n)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode DetectCycle1(ListNode head)
+        {
+            ListNode node = head;
+            List<ListNode> lists = new List<ListNode>();
+            while(node != null)
+            {
+                if (lists.Contains(node))
+                    return node;
+
+                lists.Add(node);
+                node = node.next;
+            }
+
+            return null;
+        }
+        /// <summary>
+        /// Floyd 算法
+        /// 先找相遇点，然后再找到环入口
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode DetectCycle2(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            ListNode intersect = GetIntersect(head);
+            if (intersect == null)
+                return null;
+
+            ListNode ptr1 = head;
+            ListNode ptr2 = intersect;
+            while(ptr1 != ptr2)
+            {
+                ptr1 = ptr1.next;
+                ptr2 = ptr2.next;
+            }
+            return ptr1;
+        }
+        /// <summary>
+        /// 快慢指针求相遇点
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        private ListNode GetIntersect(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while(fast != null && fast.next != null)//注意条件
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                    return slow;
+            }
+            return null;
+        }
+    }
+}
---
+++ LeetCodeTests/T142_LinkedListCycleIITests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T142_LinkedListCycleIITests
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 3, 2, 0, -4 }, 1, DisplayName = "test1")]
+        [DataRow(new int[] { 1, 2 }, 0, DisplayName = "test2")]
+        [DataRow(new int[] { 1 }, -1, DisplayName = "test3")]
+        public void DetectCycle1Test(int[] input, int expected)
+        {
+            var head = GetCycleListNode(input, expected);
+            T142_LinkedListCycleII listCycleII = new T142_LinkedListCycleII();
+            var node = listCycleII.DetectCycle2(head);
+            var pos = GetPos(head, node);
+            Assert.AreEqual(expected, pos);
+        }
+
+        public static ListNode GetCycleListNode(int[] input, int pos)
+        {
+            ListNode head = new ListNode(0);
+            ListNode tail = head;
+            ListNode node = null;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == pos)
+                    node = tail;
+
+                tail.val = input[i];
+                if (i < input.Length - 1)
+                {
+                    tail.next = new ListNode(0);
+                }
+                else if(i == input.Length - 1)
+                {
+                    tail.next = node;
+                }
+                tail = tail.next;
+            }
+            return head;
+        }
+
+        public static int GetPos(ListNode head, ListNode node)
+        {
+            int pos = 0;
+            while (head != null) 
+            {
+                if (head == node)
+                    return pos;
+                head = head.next;
+                pos++;
+            }
+            return -1;
+        }
+    }
+}
