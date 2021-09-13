990efd12885c3bbac54b9766f2070a895b487705
chao.ma <chao_ma@whu.edu.cn>
2021-09-13 23:56:17 +0800

    add t30_MinStack

---
+++ CodingInterviewChinese2/T30_MinStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T30_MinStack
+    {
+        private Stack<int> _data;
+        private Stack<int> _minData;
+        public T30_MinStack()
+        {
+            _data = new Stack<int>();
+            _minData = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            int currentMin = _minData.Count == 0 ? x : _minData.Peek();
+            if (currentMin > x)
+                currentMin = x;
+
+            _data.Push(x);
+            _minData.Push(currentMin);
+        }
+
+        public void Pop()
+        {
+            if (_data.Count != 0 && _minData.Count != 0)
+            {
+                _data.Pop();
+                _minData.Pop();
+            }
+        }
+
+        public int? Top()
+        {
+            if (_data.Count == 0)
+                return null;
+            _minData.Peek();
+            return _data.Peek();
+        }
+
+        public int? Min()
+        {
+            if (_data.Count == 0)
+                return null;
+            return _minData.Peek();
+        }
+    }
+}
---
+++ CodingInterviewChinese2Tests/T30_MinStackTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingInterviewChinese2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2.Tests
+{
+    [TestClass()]
+    public class T30_MinStackTests
+    {
+        [TestMethod()]
+        public void T30_MinStackTest()
+        {
+            T30_MinStack minStack = new T30_MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+
+            int? minData = minStack.Min();
+            Assert.AreEqual(-3, minData);
+
+            minStack.Pop();
+            int? top = minStack.Top();
+            Assert.AreEqual(0, top);
+
+            minData = minStack.Min();
+            Assert.AreEqual(-2, minData);
+        }
+    }
+}
