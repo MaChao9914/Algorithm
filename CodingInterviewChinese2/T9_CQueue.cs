2a56cb0cb7bde30ef56281c8b8ce595104c4e76b
chao.ma <chao_ma@whu.edu.cn>
2021-09-13 22:44:00 +0800

    add T9_CQueue

---
+++ CodingInterviewChinese2/T9_CQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T9_CQueue
+    {
+        private Stack<int> _stack1;
+        private Stack<int> _stack2;
+
+        public T9_CQueue()
+        {
+            _stack1 = new Stack<int>();
+            _stack2 = new Stack<int>();
+        }
+
+        public void AppendTail(int value)
+        {
+            _stack1.Push(value);
+        }
+
+        public int DeleteHead()
+        {
+            if (_stack2.Count == 0)
+            {
+                while (_stack1.Count !=0)
+                {
+                    _stack2.Push(_stack1.Pop());
+                }
+            }
+
+            if (_stack2.Count == 0)
+            {
+                return -1;
+            }
+
+            return _stack2.Pop();
+        }
+    }
+}
