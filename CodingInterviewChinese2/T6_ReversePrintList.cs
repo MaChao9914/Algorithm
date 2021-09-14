a81230b80941392d2f41b872b7457af91a5b8404
chao.ma <chao_ma@whu.edu.cn>
2021-09-14 21:06:58 +0800

    add T6_ReversePrintList

---
+++ CodingInterviewChinese2/T6_ReversePrintList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T6_ReversePrintList
+    {
+        public int[] ReversePrint(ListNode head)
+        {
+            List<int> ans = new List<int>();
+            if (head == null)
+                return ans.ToArray();
+            var temp = ReversePrint(head.next);
+            if (temp != null && temp.Length != 0)
+                ans.AddRange(temp);
+            ans.Add(head.val);
+            return ans.ToArray();
+        }
+
+        public int[] ReversePrint2(ListNode head)
+        {
+            Stack<int> ans = new Stack<int>();
+
+            if (head != null)
+            {
+                ans.Push(head.val);
+                while (head.next != null)
+                {
+                    head = head.next;
+                    ans.Push(head.val);
+                }
+            }
+            return ans.ToArray();
+        }
+    }
+}
---
+++ CodingInterviewChinese2Tests/T6_ReversePrintListTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingInterviewChinese2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2.Tests
+{
+    [TestClass()]
+    public class T6_ReversePrintListTests
+    {
+        [TestMethod()]
+        public void ReversePrintTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
