d7a6754af267e3e8cf2afdedfb0f68fca09d2b86
chao.ma <chao_ma@whu.edu.cn>
2021-09-16 20:53:56 +0800

    add t03_FindRepeadNumber

---
+++ CodingInterviewChinese2/T03_FindRepeatNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T03_FindRepeatNumber
+    {
+        public static int FindRepeatNumber1(int[] nums)
+        {
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] < 0 || nums[i] > length - 1)
+                    return -1;
+            }
+
+            int j = 0;
+            while (true)
+            {
+                if (j >= length)
+                    return -1;
+
+                int temp = nums[j];
+                if (temp == j)
+                {
+                    j++;
+                    continue;
+                }
+
+                if (temp == nums[temp])
+                    return temp;
+
+                nums[j] = nums[temp];
+                nums[temp] = temp;
+            }
+        }
+
+        public static int FindRepeatNumber2(int[] nums)
+        {
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] < 0 || nums[i] > length - 1)
+                    return -1;
+            }
+        }
+    }
+}
---
+++ CodingInterviewChinese2Tests/T03_FindRepeatNumberTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingInterviewChinese2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2.Tests
+{
+    [TestClass()]
+    public class T03_FindRepeatNumberTests
+    {
+        [TestMethod()]
+        [DataRow(new int[] { 2, 3, 1, 0, 2, 5, 3 }, new int[] {2, 3})]
+        public void FindRepeatNumber1Test(int[] input, int[] expected)
+        {
+            var r = T03_FindRepeatNumber.FindRepeatNumber1(input);
+
+            var actual = expected.Any(i => i == r);
+            Assert.IsTrue(actual);
+        }
+    }
+}
