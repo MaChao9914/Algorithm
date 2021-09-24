72ec7334cb5eb595c074939110103383c6ea3abc
chao.ma <chao_ma@whu.edu.cn>
2021-09-24 21:04:31 +0800

    add T28_IsSymmetric

---
+++ CodingInterviewChinese2/T28_IsSymmetric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T28_IsSymmetric
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            return IsSymmetric(root, root);
+        }
+
+        private bool IsSymmetric(TreeNode A, TreeNode B)
+        {
+            if (A == null && B == null)
+                return true;
+            if (A == null || B == null)
+                return false;
+            if (A.val != B.val)
+                return false;
+
+            return IsSymmetric(A.left, B.right) && IsSymmetric(A.right, B.left);
+        }
+    }
+}
