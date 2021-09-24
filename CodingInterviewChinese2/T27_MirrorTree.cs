0fc8dc4f1610b6469cd8377d0a58c26600cd4d0f
chao.ma <chao_ma@whu.edu.cn>
2021-09-24 20:52:18 +0800

    T27_MirrorTree

---
+++ CodingInterviewChinese2/T27_MirrorTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T27_MirrorTree
+    {
+        public TreeNode MirrorTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            MirrorTree(root.left);
+            MirrorTree(root.right);
+
+            return root;
+        }
+    }
+}
