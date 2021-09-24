7b2fbf9ce86e25b60fe4b13aae7e31096631a0e4
chao.ma <chao_ma@whu.edu.cn>
2021-09-24 22:28:34 +0800

    add T10_2_Fibonacci

---
+++ CodingInterviewChinese2/T10_2_Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T10_2_Fibonacci
+    {
+        public int NumWays(int n)
+        {
+            if (n == 0)
+                return 1;
+            if (n <= 2)
+                return n;
+            const int MOD = 1000000007;
+            int temp1 = 1;
+            int temp2 = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int result = (temp1 + temp2) % MOD;
+                temp1 = temp2;
+                temp2 = result;
+            }
+            return temp2;
+        }
+    }
+}
