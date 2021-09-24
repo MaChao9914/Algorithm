032389a9df06f6cb5657be57ddaf0fb19b880258
chao.ma <chao_ma@whu.edu.cn>
2021-09-24 22:16:32 +0800

    add T10_1_Fibonacci

---
+++ CodingInterviewChinese2/T10_1_Fibonacci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T10_1_Fibonacci
+    {
+        public static int Fib(int n)
+        {
+            if (n < 2)
+                return n;
+            const int MOD = 1000000007;
+            int temp1 = 0;
+            int temp2 = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                int result = (temp1 + temp2) % MOD;
+                temp1 = temp2;
+                temp2 = result;
+            }
+            return temp2;
+        }
+
+        public int Fib2(int n)
+        {
+            const int MOD = 1000000007;
+            if (n < 2)
+            {
+                return n;
+            }
+            int p = 0, q = 0, r = 1;
+            for (int i = 2; i <= n; ++i)
+            {
+                p = q;
+                q = r;
+                r = (p + q) % MOD;
+            }
+            return r;
+        }
+    }
+}
---
+++ CodingInterviewChinese2Tests/T10_1_FibonacciTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingInterviewChinese2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2.Tests
+{
+    [TestClass()]
+    public class T10_1_FibonacciTests
+    {
+        [TestMethod()]
+        [DataRow(2,1)]
+        public void FibTest(int input, int expected)
+        {
+            var r = T10_1_Fibonacci.Fib(input);
+            Assert.AreEqual(expected, r);
+        }
+    }
+}
