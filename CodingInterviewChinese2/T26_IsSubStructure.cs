b96d45a128784908cd3b6b6cf4d6fb273e12fc1c
chao.ma <chao_ma@whu.edu.cn>
2021-09-24 18:32:32 +0800

    add T26_IsSubStructure

---
+++ CodingInterviewChinese2/T26_IsSubStructure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class T26_IsSubStructure
+    {
+        public bool IsSubStructure(TreeNode A, TreeNode B)
+        {
+            if (A == null || B == null)
+                return false;
+
+            bool result = false;
+
+            if (A.val == B.val)
+                result = IsTreeAHasTreeB(A, B);
+            if (!result)
+                result = IsSubStructure(A.left, B);
+            if (!result)
+                result = IsSubStructure(A.right, B);
+            return result;
+        }
+
+        private bool IsTreeAHasTreeB(TreeNode A, TreeNode B)
+        {
+            if (B == null)
+                return true;
+            if (A == null)
+                return false;
+            if (A.val != B.val)
+                return false;
+
+            return IsTreeAHasTreeB(A.left, B.left) && IsTreeAHasTreeB(A.right, B.right);
+        }
+    }
+}
