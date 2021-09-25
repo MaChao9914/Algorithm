d6145ef8fc8f195db3f30220881f030f993582e1
chao.ma <chao_ma@whu.edu.cn>
2021-09-25 15:37:09 +0800

    add T47_MaxValue

---
+++ CodingInterviewChinese2/T47_MaxValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T47_MaxValue
+    {
+        public int MaxValue(int[][] grid)
+        {
+            int m = grid.Length;
+            if (m == 0)
+                return 0;
+            int n = grid[0].Length;
+            if (n == 0)
+                return 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+                    else if (i == 0)
+                        grid[i][j] += grid[i][j - 1];
+                    else if (j == 0)
+                        grid[i][j] += grid[i - 1][j];
+                    else
+                        grid[i][j] += Math.Max(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+            return grid[m - 1][n - 1];
+        }
+    }
+}
--- CodingInterviewChinese2Tests/T03_FindRepeatNumberTests.cs
+++ CodingInterviewChinese2Tests/T03_FindRepeatNumberTests.cs
@@ -15,10 +15,25 @@ namespace CodingInterviewChinese2.Tests
         [DataRow(new int[] { 2, 3, 1, 0, 2, 5, 3 }, new int[] {2, 3})]
         public void FindRepeatNumber1Test(int[] input, int[] expected)
         {
-            var r = T03_FindRepeatNumber.FindRepeatNumber1(input);
+            int[][] array1 =
+                    {
+                        new int[] { 1, 3, 5, 7, 9 },
+                        new int[] { 0, 2, 4, 6 },
+                        new int[] { 11, 22 }
+                    };
+            int r1 = array1.Length;
+            int c1 = array1[0].Length;
 
-            var actual = expected.Any(i => i == r);
-            Assert.IsTrue(actual);
+            int[,] array2 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            int r2 = array2.Length;
+            int rank = array2.Rank;
+            r2 = array2.GetLength(0);
+            r2 = array2.GetLength(1);
+
+            //var r = T03_FindRepeatNumber.FindRepeatNumber1(input);
+
+            //var actual = expected.Any(i => i == r);
+            //Assert.IsTrue(actual);
         }
     }
 }
