6f2e19ac94279cc4718f76c883bc78920b9ea1e1
chao.ma <chao_ma@whu.edu.cn>
2021-09-25 21:58:30 +0800

    add T18_DeleteNode

---
+++ CodingInterviewChinese2/T18_DeleteNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T18_DeleteNode
+    {
+        public ListNode DeleteNode(ListNode head, int val)
+        {
+            if (head == null)
+                return null;
+
+            if (head.val == val)
+                return head.next;
+
+            ListNode node = head;
+            while (node.next != null)
+            {
+                var nextNode = node.next;
+                if (nextNode.val == val)
+                {
+                    node.next = nextNode.next;
+                }
+                node = node.next;
+                if (node == null)
+                    break;
+            }
+
+            return head;
+        }
+    }
+}
