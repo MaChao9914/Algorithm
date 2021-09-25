e541e54e7b16b4e9ced2751e004270422e5820ad
chao.ma <chao_ma@whu.edu.cn>
2021-09-25 22:11:08 +0800

    add T22_GetKthFromEnd

---
+++ CodingInterviewChinese2/T22_GetKthFromEnd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T22_GetKthFromEnd
+    {
+        public ListNode GetKthFromEnd(ListNode head, int k)
+        {
+            if (head == null)
+                return null;
+
+            ListNode p1 = head, p2 = head;
+            for (int i = 1; i < k; i++)
+            {
+                p2 = p2.next;
+                if (p2 == null)
+                    return null;
+            }
+            while(p2.next != null)
+            {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            return p1;
+        }
+    }
+}
