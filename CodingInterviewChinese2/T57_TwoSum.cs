9319ce5b28a2b5197b117615a40fac42706956b4
chao.ma <chao_ma@whu.edu.cn>
2021-10-03 13:24:03 +0800

    add T57_TwoSum

---
+++ CodingInterviewChinese2/T57_TwoSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T57_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            List<int> ans = new List<int>();
+            int i = 0, j = nums.Length - 1;
+
+            while (i <= j)
+            {
+                int temp = nums[i] + nums[j];
+                if (temp == target)
+                {
+                    ans.Add(nums[i]);
+                    ans.Add(nums[j]);
+                    break;
+                }
+                else if (temp > target)
+                {
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return ans.ToArray();
+        }
+    }
+}
