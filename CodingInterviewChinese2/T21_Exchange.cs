851693a60945ecbf789acc36d875ac1ea3aa2b86
chao.ma <chao_ma@whu.edu.cn>
2021-10-03 11:35:36 +0800

    add T21_Exchange

---
+++ CodingInterviewChinese2/T21_Exchange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T21_Exchange
+    {
+        public int[] Exchange(int[] nums)
+        {
+            int length = nums.Length;
+
+            for (int i = 0, j = length - 1; i < j;)
+            {
+                while (i < j && nums[i] % 2 != 0)
+                    i++;
+
+                while (i < j && nums[j] % 2 == 0)
+                    j--;
+                if(i < j)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
