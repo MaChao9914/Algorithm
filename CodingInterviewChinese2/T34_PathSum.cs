1ff64d27c5a3c378b9b9103b3ecdbaaf2885b6c2
chao.ma <chao_ma@whu.edu.cn>
2021-10-03 21:29:30 +0800

    add T34_PathSum

---
+++ CodingInterviewChinese2/T34_PathSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T34_PathSum
+    {
+        IList<IList<int>> _ans = new List<IList<int>>();
+        List<int> _path = new List<int>();
+        public IList<IList<int>> PathSum(TreeNode root, int target)
+        {
+            if (root != null)
+                GetPath(root, target);
+            return _ans;
+        }
+
+        void GetPath(TreeNode root, int target)
+        {
+            target -= root.val;
+            _path.Add(root.val);
+            if (target == 0 && root.left == null && root.right == null)
+            {
+                _ans.Add(_path.ToArray());
+                return;
+            }
+            if(root.left != null)
+                GetPath(root.left, target);
+
+            if(root.right != null)
+                GetPath(root.right, target);
+            _path.RemoveAt(_path.Count - 1);
+
+        }
+    }
+}
