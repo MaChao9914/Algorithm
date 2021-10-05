d28d90de6d4a04b037ad20e87b6295b0d24274f8
chao.ma <chao_ma@whu.edu.cn>
2021-10-05 22:55:37 +0800

    add T64_SumNums

---
+++ CodingInterviewChinese2/T64_SumNums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T64_SumNums
+    {
+        public int SumNums(int n)
+        {
+            //if (n == 1)
+            //    return n;
+            //return n + SumNums(n - 1);
+
+            bool isRecur = n >= 1 && (n += SumNums(n - 1)) >= 1;
+            return n;
+
+
+        }
+    }
+}
