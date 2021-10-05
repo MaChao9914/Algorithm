1232cfdfc392bcbf2aadf8d9867cda8b366b8efe
chao.ma <chao_ma@whu.edu.cn>
2021-10-05 11:33:51 +0800

    add T45_MinNumber

---
+++ CodingInterviewChinese2/T45_MinNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T45_MinNumber
+    {
+        public string MinNumber1(int[] nums)
+        {
+            string[] strs = Array.ConvertAll(nums, i => i.ToString());
+
+            Array.Sort(strs, (x, y) => (x + y).CompareTo(y + x));
+
+            return string.Concat(strs);
+        }
+    }
+}
