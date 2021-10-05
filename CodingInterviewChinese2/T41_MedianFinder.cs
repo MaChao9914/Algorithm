443cd29beb7904f4d1bf5c9af690931449a52d03
chao.ma <chao_ma@whu.edu.cn>
2021-10-05 20:14:20 +0800

    add T41_MedianFinder

---
+++ CodingInterviewChinese2/T41_MedianFinder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    public class MedianFinder
+    {
+        //List<int> _mins;//存较小的一半
+        //List<int> _bigs;//
+        List<int> _datas;
+        /** initialize your data structure here. */
+        public MedianFinder()
+        {
+            //_mins = new List<int>();
+            //_bigs = new List<int>();
+            _datas = new List<int>();
+        }
+
+        public void AddNum(int num)
+        {
+            _datas.Add(num);
+            _datas.Sort();
+            //if (_mins.Count == _bigs.Count)
+            //{
+            //    //_mins.Add(num);
+            //    //_mins.Sort((x, y) => y - x);
+            //    //_bigs.Add(_mins[0]);
+            //    //_bigs.Sort();
+            //    //_mins.RemoveAt(0);
+            //    _mins.Add(num);
+            //    int m = _mins.Max();
+            //    _bigs.Add(m);
+            //    _mins.Remove(m);
+            //}
+            //else
+            //{
+            //    //_bigs.Add(num);
+            //    //_bigs.Sort();
+            //    //_mins.Add(_bigs[0]);
+            //    //_mins.Sort((x, y) => y - x);
+            //    //_bigs.RemoveAt(0);
+            //    _bigs.Add(num);
+            //    int t = _bigs.Min();
+            //    _mins.Add(t);
+            //    _bigs.Remove(t);
+            //}
+        }
+
+        public double FindMedian()
+        {
+            //return _mins.Count == _bigs.Count ? (_mins.Max() + _bigs.Min()) / 2.0 : _bigs.Min();
+            int l = _datas.Count;
+            return l % 2 == 0 ? (_datas[l / 2] + _datas[(l - 1) / 2]) / 2.0 : _datas[l / 2];
+        }
+    }
+}
--- CodingInterviewChinese2Tests/T03_FindRepeatNumberTests.cs
+++ CodingInterviewChinese2Tests/T03_FindRepeatNumberTests.cs
@@ -15,20 +15,24 @@ namespace CodingInterviewChinese2.Tests
         [DataRow(new int[] { 2, 3, 1, 0, 2, 5, 3 }, new int[] {2, 3})]
         public void FindRepeatNumber1Test(int[] input, int[] expected)
         {
-            int[][] array1 =
-                    {
-                        new int[] { 1, 3, 5, 7, 9 },
-                        new int[] { 0, 2, 4, 6 },
-                        new int[] { 11, 22 }
-                    };
-            int m1 = array1.Length;
-            int n1 = array1[0].Length;
+            var a = input.ToList();
+            var b = input.ToList();
+            a.Sort();
+            b.Sort((x, y) => y - x);
+            //int[][] array1 =
+            //        {
+            //            new int[] { 1, 3, 5, 7, 9 },
+            //            new int[] { 0, 2, 4, 6 },
+            //            new int[] { 11, 22 }
+            //        };
+            //int m1 = array1.Length;
+            //int n1 = array1[0].Length;
 
-            int[,] array2 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
-            int r2 = array2.Length;
-            int rank = array2.Rank;
-            int m2 = array2.GetLength(0);
-            int n2 = array2.GetLength(1);
+            //int[,] array2 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            //int r2 = array2.Length;
+            //int rank = array2.Rank;
+            //int m2 = array2.GetLength(0);
+            //int n2 = array2.GetLength(1);
 
             //var r = T03_FindRepeatNumber.FindRepeatNumber1(input);
 
