c6c5005e431c22d65e04bc13e62ba96c846bee1a
chao.ma <chao_ma@whu.edu.cn>
2021-10-05 20:24:49 +0800

    add T55_1_MaxDepth

---
+++ CodingInterviewChinese2/T55_1_MaxDepth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T55_1_MaxDepth
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int left = 1 + MaxDepth(root.left);
+            int right = 1 + MaxDepth(root.right);
+            return left > right ? left : right;
+        }
+    }
+}
