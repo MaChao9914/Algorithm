5d31595813a8d7d63ce98616ff33acaeeb2f484a
chao.ma <chao_ma@whu.edu.cn>
2021-10-05 14:23:13 +0800

    add T40_GetLeastNumbers

--- CodingInterviewChinese2/T40_GetLeastNumbers.cs
+++ CodingInterviewChinese2/T40_GetLeastNumbers.cs
@@ -8,9 +8,67 @@ namespace CodingInterviewChinese2
 {
     class T40_GetLeastNumbers
     {
-        public int[] GetLeastNumbers(int[] arr, int k)
+        public int[] GetLeastNumbers2(int[] arr, int k)
         {
+            if (k <= 0)
+                return new int[0];
+            if (arr == null || arr.Length < k)
+                return arr;
 
+            List<int> ans = new List<int>(k);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                if(i<k)
+                {
+                    ans.Add(temp);
+                    continue;
+                }
+
+                int max = ans.Max();
+                if(temp < max)
+                {
+                    ans.Remove(max);
+                    ans.Add(temp);
+                }
+            }
+            return ans.ToArray();
+        }
+
+        public int[] GetLeastNumbers1(int[] arr, int k)
+        {
+            if (k <= 0)
+                return new int[0];
+            if (arr == null || arr.Length < k)
+                return arr;
+
+            int start = 0, end = arr.Length - 1;
+            int partition = FindPartition(arr, start, end);
+            while (partition != k -1)
+            {
+                if (partition < k - 1)
+                    partition = FindPartition(arr, start = partition + 1, end);
+                else
+                    partition = FindPartition(arr, start, end = partition - 1);
+            }
+            return arr.Take(k).ToArray();
+        }
+
+        int FindPartition(int[] arr, int start, int end)
+        {
+            int key = arr[start];
+
+            while (start < key)
+            {
+                while (start < key && arr[end] >= key)
+                    end--;
+                arr[start] = arr[end];
+                while (start < key && arr[start] <= key)
+                    start++;
+                arr[end] = arr[start];
+            }
+            arr[start] = key;
+            return start;
         }
     }
 }
