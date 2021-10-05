3a585538b9de1e9a3fe71a70bcbf65b2b68cbd7f
chao.ma <chao_ma@whu.edu.cn>
2021-10-05 13:18:47 +0800

    add T61_IsStraight

---
+++ CodingInterviewChinese2/T40_GetLeastNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T40_GetLeastNumbers
+    {
+        public int[] GetLeastNumbers(int[] arr, int k)
+        {
+
+        }
+    }
+}
---
+++ CodingInterviewChinese2/T61_IsStraight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T61_IsStraight
+    {
+        public bool IsStraight(int[] nums)
+        {
+            if (nums == null || nums.Length < 5)
+                return false;
+            int joker = 0;
+            Array.Sort(nums);
+            for (int i = 0; i < 4; i++)
+            {
+                if (nums[i] == 0)
+                    joker++;
+                else if (nums[i] == nums[i + 1])
+                    return false;
+            }
+
+            return nums[4] - nums[joker] < 5 ? true : false;
+        }
+    }
+}
