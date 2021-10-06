abe7c4f09838238d7e8ca6094617289948db5891
chao.ma <chao_ma@whu.edu.cn>
2021-10-06 15:17:06 +0800

    add T14_1_CuttingRope

---
+++ CodingInterviewChinese2/T14_1_CuttingRope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T14_1_CuttingRope
+    {
+        public int CuttingRope(int n)
+        {
+            if (n < 2)
+                return 0;
+            else if (n == 2)
+                return 1;
+            else if (n == 3)
+                return 2;
+            int max = 0;
+            int[] k = new int[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(i < 4)
+                {
+                    k[i] = i;
+                    continue;
+                }
+                for (int j = 1; j <= i/2; j++)
+                {
+                    int temp = k[j] * k[i - j];
+                    if (temp > max)
+                        max = temp;
+                    k[i] = max;
+                }
+            }
+
+            return k[n];
+        }
+    }
+}
