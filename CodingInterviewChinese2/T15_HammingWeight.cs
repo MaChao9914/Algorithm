25d82be66ac0d6cf8e281a9a4fc0e92bec4abd09
chao.ma <chao_ma@whu.edu.cn>
2021-10-17 17:04:36 +0800

    add T15_HammingWeight

---
+++ CodingInterviewChinese2/T15_HammingWeight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T15_HammingWeight
+    {
+        public int HammingWeight1(uint n)
+        {
+            int ans = 0;
+            uint flag = 1;
+            while (flag != 0)
+            {
+                if ((n & flag) != 0)
+                    ans++;
+                flag = flag << 1;
+            }
+            return ans;
+        }
+
+        /// <summary>
+        /// n & (n−1)，其预算结果恰为把 n 的二进制位中的最低位的 1 变为 0 之后的结果
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int HammingWeight2(uint n)
+        {
+            int ans = 0;
+            while (n != 0)
+            {
+                ans++;
+                n = n & (n - 1);
+            }
+            return ans;
+        }
+    }
+}
