16ab785c2da8188e1932934dcf57fc2af1e5d77d
chao.ma <chao_ma@whu.edu.cn>
2021-10-18 22:05:57 +0800

    add T16_MyPow

---
+++ CodingInterviewChinese2/T16_MyPow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewChinese2
+{
+    class T16_MyPow
+    {
+        public double MyPow1(double x, int n)
+        {
+            double ans = x;
+            if (x == 0)
+                return ans;
+            if (x == 1 || n == 0)
+                return 1;
+
+            for (int i = 1; i < Math.Abs(n); i++)
+            {
+                ans = ans * x;
+            }
+
+            if (n < 0)
+                return 1 / ans;
+            return ans;
+        }
+
+        public double MyPow2(double x, int n)
+        {
+            if (x == 0)
+                return 0;
+            if (n == 0)
+                return 1;
+            long b = n;
+            if (b < 0)
+            {
+                x = 1 / x;
+                b = -b;
+            }
+
+            double ans = 1.0;//当 n = -2147483648 时执行 n = -n 会因越界而赋值出错。解决方法是先将 n 存入 long 变量 b ，后面用 b 操作即可
+            while (b > 0)
+            {
+                if ((b & 1) == 1)
+                    ans *= x;
+                x = x * x;
+                b = b >> 1;
+            }
+            
+            return ans;
+        }
+    }
+}
