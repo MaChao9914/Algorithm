01fe8d1645232b5cd883e5c146073a4152e246e4
chao.ma <chao_ma@whu.edu.cn>
2021-12-05 22:09:37 +0800

    add T27_RemoveElement

---
+++ LeetCode/T27_RemoveElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class T27_RemoveElement
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int l = 0;
+            int r = nums.Length - 1;
+            while (l <= r)
+            {
+                if (nums[l] == val)
+                {
+                    nums[l] = nums[r];
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+            return l;
+        }
+    }
+}
---
+++ LeetCodeTests/T27_RemoveElementTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class T27_RemoveElementTests
+    {
+        [TestMethod()]
+        [DataRow(new int[] {3,2,2,3}, 3)]
+        public void RemoveElementTest(int[] nums, int val)
+        {
+            T27_RemoveElement.RemoveElement(nums, val);
+        }
+    }
+}
