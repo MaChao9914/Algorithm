041685f19550c4d2059f874e16b14d4d439e3489
chao.ma <chao_ma@whu.edu.cn>
2021-12-05 21:12:20 +0800

    add T86_Partition

---
+++ LeetCode/T86_Partition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class T86_Partition
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode node1 = new ListNode();
+            ListNode pHead1 = node1;
+            ListNode node2 = new ListNode();
+            ListNode pHead2 = node2;
+
+            while (head != null)
+            {
+                if (head.val < x)
+                {
+                    node1.next = head;
+                    node1 = node1.next;
+                }
+                else 
+                {
+                    node2.next = head;
+                    node2 = node2.next;
+                }
+                head = head.next;
+            }
+            node2.next = null;
+            node1.next = pHead2.next;
+            return pHead1.next;
+        }
+    }
+}
