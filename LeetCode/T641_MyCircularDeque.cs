e6de3d0299fed312bc863d6420d0e05ef7f61b0f
chao.ma <chao_ma@whu.edu.cn>
2021-12-08 23:07:29 +0800

    add T641_MyCircularDeque

---
+++ LeetCode/T641_MyCircularDeque.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MyCircularDeque
+    {
+        private int[] _queue;
+        private int _headIndex = 0, _capacity = 0, _count = 0;
+        public MyCircularDeque(int k)
+        {
+            _queue = new int[k];
+            _capacity = k;
+        }
+
+        public bool InsertFront(int value)
+        {
+            if (IsFull())
+                return false;
+            _headIndex = (_headIndex + _capacity - 1) % _capacity;
+            _queue[_headIndex] = value;
+            this._count++;
+            return true;
+        }
+
+        public bool InsertLast(int value)
+        {
+            if (IsFull())
+                return false;
+            _queue[(_headIndex + _count) % _capacity] = value;
+            this._count++;
+            return true;
+        }
+
+        public bool DeleteFront()
+        {
+            if (IsEmpty())
+                return false;
+            _headIndex = (_headIndex + 1) % _capacity;
+            _count--;
+            return true;
+        }
+
+        public bool DeleteLast()
+        {
+            if (IsEmpty())
+                return false;
+            _count--;
+            return true;
+        }
+
+        public int GetFront()
+        {
+            if (IsEmpty())
+                return -1;
+            return _queue[_headIndex];
+        }
+
+        public int GetRear()
+        {
+            if (IsEmpty())
+                return -1;
+            return _queue[(_headIndex + _count - 1) % _capacity];
+        }
+
+        public bool IsEmpty()
+        {
+            return _count == 0;
+        }
+
+        public bool IsFull()
+        {
+            return _count == _capacity;
+        }
+    }
+}
