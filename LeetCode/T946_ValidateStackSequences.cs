7b039eb5d63fc9a529af6148f617f89d4e07ce82
chao.ma <chao_ma@whu.edu.cn>
2021-12-11 17:21:17 +0800

    add T946_ValidateStackSequences

---
+++ LeetCode/T946_ValidateStackSequences.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class T946_ValidateStackSequences
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            Stack<int> stack = new Stack<int>();
+            int i = 0;
+            foreach (var item in pushed)
+            {
+                stack.Push(item);
+                while (stack.Count != 0 && stack.Peek() == popped[i])
+                {
+                    stack.Pop();
+                    i++;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
