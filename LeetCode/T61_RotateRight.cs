801f30fc53f5c576e43b09ad3929f2c81879f23c
chao.ma <chao_ma@whu.edu.cn>
2021-12-12 11:20:16 +0800

    add T61_RotateRight

---
+++ LeetCode/T61_RotateRight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class T61_RotateRight
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || k == 0 || head.next == null)
+                return head;
+
+            int length = 1;
+            int move = 0;
+            ListNode node = head;
+            while (node.next != null)
+            {
+                length++;
+                node = node.next;
+            }
+            move = k > length ? k % length : k;
+            node.next = head;
+            for (int i = 0; i < length - move; i++)
+            {
+                node = node.next;
+            }
+            ListNode ans = node.next;
+            node.next = null;
+            return ans;
+        }
+    }
+}
