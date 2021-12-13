6c71418648504dd753448de36edd1c8dc2184588
chao.ma <chao_ma@whu.edu.cn>
2021-12-13 22:08:23 +0800

    add T105_BuildTree

---
+++ LeetCode/T105_BuildTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class T105_BuildTree
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder.Length == 0 || inorder.Length == 0)
+                return null;
+            int rootValue = preorder[0];
+            int index = Array.IndexOf(inorder, rootValue);
+            TreeNode tree = new TreeNode(rootValue);
+            tree.left = BuildTree(preorder.Skip(1).Take(index).ToArray(), inorder.Take(index).ToArray());
+            tree.right = BuildTree(preorder.Skip(index + 1).ToArray(), inorder.Skip(index + 1).ToArray());
+            return tree;
+        }
+    }
+}
