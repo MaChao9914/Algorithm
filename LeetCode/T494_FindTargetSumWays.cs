bca25ba4af2e2dda687caa78721f840864b3bdae
chao.ma <chao_ma@whu.edu.cn>
2021-12-15 21:28:53 +0800

    add T494_FindTargetSumWays

---
+++ LeetCode/T494_FindTargetSumWays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class T494_FindTargetSumWays
+    {
+        private int _count = 0;
+        public int FindTargetSumWays(int[] nums, int target)
+        {
+            Backtrack(nums, target, 0);
+            return _count;
+        }
+
+        private void Backtrack(int[] nums, int target, int index)
+        {
+            if (index == nums.Length)
+            {
+                if (target == 0)
+                    _count++;
+            }
+            else
+            {
+                Backtrack(nums, target - nums[index], index + 1);
+                Backtrack(nums, target + nums[index], index + 1);
+            }
+        }
+    }
+}
