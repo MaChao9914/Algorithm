c239b7706b9d3907e1e68ea5d54206a7ccbcdf75
chao.ma <chao_ma@whu.edu.cn>
2021-12-15 20:05:36 +0800

    add T112_HasPathSum

---
+++ LeetCode/T112_HasPathSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class T112_HasPathSum
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return false;
+            if (targetSum == root.val && root.left == null && root.right == null)
+                return true;
+            
+            return HasPathSum(root.left, targetSum - root.val)
+                   || HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
