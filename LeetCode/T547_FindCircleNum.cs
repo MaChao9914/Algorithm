08d269398947fa6da88b674ee9aeb512992e7734
chao.ma <chao_ma@whu.edu.cn>
2021-12-15 22:26:29 +0800

    add T547_FindCircleNum

---
+++ LeetCode/T547_FindCircleNum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class T547_FindCircleNum
+    {
+        public int FindCircleNum(int[][] isConnected)
+        {
+            int cities = isConnected.Length;
+            bool[] visited = new bool[cities];
+            int cirleNum = 0;
+            for (int i = 0; i < cities; i++)
+            {
+                if (!visited[i])
+                {
+                    FindCircleNum(isConnected, visited, cities, i);
+                    cirleNum++;
+                }
+            }
+            return cirleNum;
+        }
+
+        private void FindCircleNum(int[][] isConnected, bool[] visited, int cities, int currIndex)
+        {
+            for (int i = 0; i < cities; i++)
+            {
+                if(isConnected[currIndex][i] == 1 && !visited[i])
+                {
+                    visited[i] = true;
+                    FindCircleNum(isConnected, visited, cities, i);
+                }
+            }
+        }
+    }
+}
