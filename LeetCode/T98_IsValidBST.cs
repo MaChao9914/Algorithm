e2b77dfc75b9eafc3b456c9bc621a4d49da8942b
chao.ma <chao_ma@whu.edu.cn>
2021-12-15 20:39:05 +0800

    add T98_IsValidBST

---
+++ LeetCode/T98_IsValidBST.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class T98_IsValidBST
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            if (root == null)
+                return true;
+
+            return IsValidBST(root, long.MinValue, long.MaxValue);
+        }
+
+        private bool IsValidBST(TreeNode root, long lower, long upper)
+        {
+            if (root == null)
+                return true;
+
+            if (root.val <= lower || root.val >= upper)
+                return false;
+
+            return IsValidBST(root.left, lower, root.val)
+                   && IsValidBST(root.right, root.val, upper);
+        }
+    }
+}
